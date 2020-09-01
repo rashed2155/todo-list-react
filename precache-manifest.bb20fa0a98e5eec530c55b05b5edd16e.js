bdcbd6798aa6191af2bee5f9ad7c1221d927de94
Rashed Khan <abir97bd@gmail.com>
2020-09-01 23:53:24 +0600

    Updates

---
+++ precache-manifest.bb20fa0a98e5eec530c55b05b5edd16e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c03fe439cbbf44443495f55726077bcc",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "a5fdfb87b96586b354cf",
+    "url": "/todo-list-react/static/css/main.331058b0.chunk.css"
+  },
+  {
+    "revision": "dc7864cc3cc6b7a195d7",
+    "url": "/todo-list-react/static/js/2.fd227c02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-react/static/js/2.fd227c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5fdfb87b96586b354cf",
+    "url": "/todo-list-react/static/js/main.708bdcac.chunk.js"
+  },
+  {
+    "revision": "826642a5112903c57c77",
+    "url": "/todo-list-react/static/js/runtime-main.22aeb5a4.js"
+  }
+]);
